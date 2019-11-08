3ac4b48fc7bd58a412dd1b4e856cbde38d23bffd
Mohammad-Alghrawe <mohammad.alghrawe@gmail.com>
2019-11-09 00:50:36 +0100

    some exp

---
+++ ContactForm/app.js
@@ -0,0 +1,40 @@
+console.log('js is running..');
+
+
+// Initialize Firebase
+var firebaseConfig = {}
+firebase.initializeApp(firebaseConfig);
+//reference messages collection .
+var messagesRef = firebase.database().ref('messages');
+// Listen for form submit
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+// Submit form
+function submitForm(e) {
+    e.preventDefault();
+    // Get values
+    var name = getInputVal('name');
+    var company = getInputVal('company');
+    var email = getInputVal('email');
+    var phone = getInputVal('phone');
+    var message = getInputVal('message');
+    // save messages
+    saveMessage(name, company, email, phone, message);
+    // clear the form after submit
+    document.getElementById('contactForm').reset();
+}
+
+// Function to get get form values
+function getInputVal(id) {
+    return document.getElementById(id).value;
+}
+//Save the message to firebase...
+function saveMessage(name, company, email, phone, message) {
+    var newMessageRef = messagesRef.push();
+    newMessageRef.set({
+        name: name,
+        company: company,
+        email: email,
+        phone: phone,
+        message: message
+    });
+}
---
+++ HelloWorld/app.js
@@ -0,0 +1,6 @@
+console.log('%s%%', 90); // will print a precent%
+
+
+
+test('first ex',)
+assert.deepEqual(["ana", "mohammad", "noor"], ["1", "4", "6"]);
